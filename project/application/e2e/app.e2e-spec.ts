7355ec2d0909518ebba919623518518803d1eaee
athlonUA <alexgarmatenko@gmail.com>
2017-08-17 13:06:21 +0300

    fixed tests

--- project/application/e2e/app.e2e-spec.ts
+++ project/application/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ng-app App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Welcome to app!!');
+        expect(<any>page.getParagraphText()).toEqual('Welcome to app!!');
     });
 });
